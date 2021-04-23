17d8303413ed93d68d5ed414c709a1dc5ef2075d
Ashish Verma <ashishkv.2016@gmail.com>
2021-04-23 18:01:36 +0530

    Added supplementary files

---
+++ main.js
@@ -0,0 +1,36 @@
+function MensWear(){
+  window.location="MensWear.html";
+}
+function WomensWear(){
+  window.location="WomensWear.html";
+}
+function KidsWear(){
+  window.location="KidsWear.html";
+}
+function Sports(){
+  window.location="Sports.html";
+}
+function Bags(){
+  window.location="Bags.html";
+}
+function Sunglasses(){
+  window.location="Sunglasses.html";
+}
+function Cart(){
+  window.location="Cart.html";
+}
+function Categories(){
+  window.location="Categories.html";
+}
+function TopCharts(){
+  window.location="TopCharts.html";
+}
+function Featured(){
+  window.location="Featured.html";
+}
+function Offers(){
+  window.location="Offers.html";
+}
+function BannerClick(){
+  window.location="Festival.html";
+}
